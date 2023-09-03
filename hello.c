5cc9779881ab454105eadc767c39ac9bc82c5325
Pramo20 <pramodheepa@gmail.com>
2023-09-03 09:44:57 +0530

    updated bsort

--- hello.c
+++ hello.c
@@ -1,4 +1,34 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
     printf("hello");
+}*/
+//bubble sort
+#include<stdio.h>
+#include<stdlib.h>
+void swap(int *a, int *b){
+    int temp = *a;
+    *a =* b;
+    *b = temp;
+}
+void bsort(int *arr, int size){
+    for(int i=size-1 ; i>=1;i--){ //executes n-1 times
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(&arr[j],&arr[j+1]);
+            }
+        }
+    }
+}
+int main(){
+    int n;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    int *nums =(int*)malloc(n*sizeof(int));
+    for(int i=0;i<n;i++){
+        scanf("%d",&nums[i]);
+    }
+    bsort(nums,n);
+    for(int i=0;i<n;i++){
+        printf("%d ",nums[i]);
+    }
 }
